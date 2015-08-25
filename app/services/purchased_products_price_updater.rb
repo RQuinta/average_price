21c8769341b06c300d3e6391e720da91f1ed864b
raphael <raphael_santos@id.uff.br>
2015-08-25 01:53:58 -0300

    Modificacoes no servico de atualizocao de preco

--- app/services/purchased_products_price_updater.rb
+++ app/services/purchased_products_price_updater.rb
@@ -6,23 +6,24 @@ class PurchasedProductsPriceUpdater
   end
 
   def update_price
-    purchased_products = PurchasedProduct.by_base_product(@purchased_product.base_product_id)
-    @base_product.price = self.average_price_purchased_product_calculator(purchased_products)
+    purchased_products = PurchasedProduct.by_base_product(@base_product.id)
+    @base_product.average_price = average_price_purchased_product_calculator(purchased_products)
+    @base_product.save
   end
-  handle_asynchronously :update_price,:run_at => Proc.new { 1.minutes.from_now }
+  # handle_asynchronously :update_price,:run_at => Proc.new { 1.minutes.from_now }
 
   private
 
-  def self.total_amount_products_calculator(purchased_products)
-    purchased_products.inject {|acc,product| acc += product.amount}
+  def total_amount_products_calculator(purchased_products)
+    purchased_products.inject(0) {|acc,product| acc + product.amount}
   end
 
-  def self.total_cost_purchased_product_calculator(purchased_products)
-    purchased_products.inject {|acc,product| acc += product.price * product.amount}
+  def total_cost_purchased_product_calculator(purchased_products)
+    purchased_products.inject(0) {|acc,product| acc + (product.price * product.amount)}
   end
 
   def average_price_purchased_product_calculator(purchased_products)
-    self.total_cost_purchased_product_calculator(purchased_products)/self.total_amount_products_calculator(purchased_products)
+    total_cost_purchased_product_calculator(purchased_products)/total_amount_products_calculator(purchased_products)
   end
 
 end
